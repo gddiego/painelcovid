caa8c347d9909bd20d5102062dfef9c0cb51fe07
Diego <gdmendes55@gmail.com>
2020-05-26 14:56:23 -0300

    recover

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL
+    baseURL:
+     'https://api.thevirustracker.com/free-api?countryTotal=BR'
 })
 
 export default api;
